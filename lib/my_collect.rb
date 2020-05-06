617d4cad079763c0d759c265f340b7477ae5e728
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 19:47:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   modified_collection = []
   i = 0 
   while i < collection.length 
-    modified_collection << yield collection[i]
+    modified_collection << yield(collection[i])
     i += 1 
   end
   return modified_collection
